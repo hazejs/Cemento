3f646742752c759a21b4f6fb87ff2bf2c1af2e69
hazejs <35347891+hazejs@users.noreply.github.com>
2024-01-24 08:20:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,25 +10,25 @@ function App() {
     },
     data: [
       { id: '1', name: 'John Doe', age: 25 },
-      { id: '2', name: 'Jane Smith', age: 30 },
+      { id: '2', name: 'Tomer Eden', age: 30 },
       {
         id: '3',
         name: 'Bob Johnson',
         age: 40,
         subRows: [
-          { id: '3.1', name: 'Bob Jr.', age: 10 },
-          { id: '3.2', name: 'Bobette', age: 15 },
+          { id: '3.1', name: 'Bob Marley', age: 10 },
+          { id: '3.2', name: 'The Hulk', age: 15 },
         ],
       },
-      { id: '4', name: 'Alice White', age: 28 },
-      { id: '5', name: 'Charlie Brown', age: 35 },
+      { id: '4', name: 'Spiderman', age: 28 },
+      { id: '5', name: 'Igal Antebi', age: 35 },
       {
         id: '6',
-        name: 'Eva Green',
+        name: 'Hulk Hogan',
         age: 45,
         subRows: [
-          { id: '6.1', name: 'Eva Jr.', age: 20 },
-          { id: '6.2', name: 'Evan', age: 22 },
+          { id: '6.1', name: 'Dr. Strange', age: 20 },
+          { id: '6.2', name: 'Thor', age: 22 },
         ],
       },
     ],
